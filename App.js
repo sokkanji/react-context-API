a56db85064ba6d5b410308d9c40a94810d7304e7
sokkanji <sskkanji@gmail.com>
2020-09-16 15:02:28 +0900

    #3 Feat : Asking for Permissions

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import React from "react";
+import {Alert} from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
-  }
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const {
+        coords : { latitude, logitude}
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+      console.log(coords.latitude, coords.longitude);
+    } catch (error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
 
   componentDidMount() {
     this.getLocation();
   }
-  
+
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
